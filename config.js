adf837752cf657aea9715bd87854fd60d1088cda
sonnguyen <sonnguyen@anvui.vn>
2022-08-23 15:48:58 +0700

    base prepare

---
+++ config.js
@@ -0,0 +1,10 @@
+const connectionConfig = {
+  db: {
+    host: 'db4free.net',
+    user: 'av_somebody',
+    password: 'av123123',
+    database: 'avwebbenxe'
+  },
+  listPerPage: 10
+}
+module.exports = connectionConfig
